ce4d3b8ae9d02829602dd937efa91d77969ce302
michael.lewellen <michael.lewellen@snow.edu>
2023-12-05 16:32:57 -0700

    more complete

--- Program.cs
+++ Program.cs
@@ -1,78 +1,132 @@
-﻿List<int> numbers = new List<int>(){25,90,5,100,15,80,35,60,20,40,75,55,95,50,85,30,65,10,45,70};
-List<int> hard10 = new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-List<int> hard9= new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-List<int> hard8 = new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-List<int> hard7 = new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-List<int> hard6 = new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-List<int> hard5 = new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-List<int> hard4 = new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-List<int> hard3 = new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-List<int> hard2 = new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-List<int> hard1 = new List<int>(){100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,200,300,400,500,600,700,800,900,1000,1200,1500};
-Console.Write("From 1-10, how hard do you want to spin the wheel? ");
-int selection = Convert.ToInt32(Console.ReadLine());
-List<int> selectedList = selection switch
-{
-    1=>hard1,
-    2=>hard2,
-    3=>hard3,
-    4=>hard4,
-    5=>hard5,
-    6=>hard6,
-    7=>hard7,
-    8=>hard8,
-    9=>hard9,
-    10=>hard10,
-    _=>hard10
-};
-Queue<int> myQueue = new Queue<int>(selectedList);
-
-
+﻿Random rand = new Random();
+List<int> numbers = new List<int>(){25,90,5,100,15,80,35,60,20,40,75,55,95,50,85,30,65,10,45,70};
+Console.BackgroundColor=ConsoleColor.Black;
 int start = 0;
-Console.Clear();
-while(myQueue.TryDequeue(out int result))
+int[] player = new int[3];
+for (int i = 1; i<=3; i++)
 {
-    Console.SetCursorPosition(0,0);
-    int count = start;
-    bool tick = true;
-    (int left, int top) position;
-    string buffer = "";
-    position =  Console.GetCursorPosition();
-    tick = drawArrow(tick,position);
-    for(int i = 0; i<7; i++)
+    Console.Clear();
+    int firstSpin = 0;
+    int secondSpin = 0;
+    int total = 0;
+    Console.Write($"Player {i}, from 1-10, how hard do you want to spin the wheel? ");
+    int selection = Convert.ToInt32(Console.ReadLine());
+    firstSpin = SpinWheel(start, selection, numbers);
+    start = numbers.IndexOf(firstSpin)-3;
+    if (start < 0)
+        start +=20;
+    Console.Write($"You spun {firstSpin}, do you want to spin again? ");
+    char response = Console.ReadKey(true).KeyChar;
+    Console.WriteLine(response);
+    if (response == 'y' || response == 'Y')
     {
-        Console.GetCursorPosition();
-        Console.BackgroundColor = ConsoleColor.Black;
-        buffer = getBuffer(numbers[count]);
-        if(numbers[count] == 100)
-        {
-            Console.BackgroundColor = ConsoleColor.Red;
-        }
-        else if(numbers[count] == 5 || numbers[count] == 15)
-            Console.BackgroundColor = ConsoleColor.Green;
-        else if (count % 2 == 0)
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-        Console.WriteLine("     " + numbers[count]+ buffer);
-        count ++;
-        if (count >= 20)
-            count = count-20;
+        Console.Write($"Player {i}, from 1-10, how hard do you want to spin the wheel? ");
+        selection = Convert.ToInt32(Console.ReadLine());
+        secondSpin = SpinWheel(start,selection, numbers);
+        start = numbers.IndexOf(secondSpin)-3;
+        if (start < 0)
+            start +=20;
     }
-    Thread.Sleep(result);
-    start ++;
-    if (start >= 20)
-        start = start - 20;
+    total = firstSpin + secondSpin;
+    if (total > 100 )
+    {
+        Console.WriteLine("\nYou went over.");
+        total = 0;         
+    }
+    else if (total < 100)
+    {
+        Console.WriteLine($"\nYour total is {total}");        
+    }
+    player[i-1] = total;
+    displayScores(player);
 }
-int number = 0;
-if (start + 2 >= 20)
-    number = start + 2 - 20;
-else    
-    number = start + 2;
+for (int i = 0; i<3; i++)
+{
+    if (player[i] == 100)
+    {
+        
+    }
+}
+static void displayScores(int[] player)
+{
+    Console.SetCursorPosition(0,10);
+    Console.WriteLine("Current Standings:");
+    for (int i = 0; i<3; i ++)
+    {
+        Console.WriteLine($"Player {i+1}: {player[i]}");
+    }
+    Console.WriteLine("Press any key to continue");
+    Console.ReadKey();
+}
+static int SpinWheel(int start, int selection, List<int> numbers)
+{
+    Random rand = new Random();
+    List<int> slowDown = new List<int>(){100,200,300,400,500,600,700,800,900,1000,1200,1500};
+    Console.BackgroundColor= ConsoleColor.Black;
+    Console.Clear();
+    int difficultyFactor = selection switch
+    {
+        1=>rand.Next(0,9),
+        2=>rand.Next(10,19),
+        3=>rand.Next(20,29),
+        4=>rand.Next(30,39),
+        5=>rand.Next(40,49),
+        6=>rand.Next(50,59),
+        7=>rand.Next(60,69),
+        8=>rand.Next(70,79),
+        9=>rand.Next(80,99),
+        10=>rand.Next(90,99),
+        _=>rand.Next(40,49)    
+    };
+    Queue<int> myQueue = new Queue<int>();
+    for(int i = 0; i<difficultyFactor + 8; i ++)
+        myQueue.Enqueue(50);
+    foreach (int x in slowDown)
+        myQueue.Enqueue(x);
 
-Console.SetCursorPosition(0,10);
-Console.WriteLine($"You spun {numbers[number]}");
-
-
-string getBuffer(int num)
+        Console.BackgroundColor=ConsoleColor.Black;
+        Console.Clear();
+    while(myQueue.TryDequeue(out int result))
+    {
+        Console.SetCursorPosition(0,0);
+        int count = start;
+        bool tick = true;
+        (int left, int top) position;
+        string buffer = "";
+        position =  Console.GetCursorPosition();
+        tick = drawArrow(tick,position);
+        for(int i = 0; i<7; i++)
+        {
+            Console.GetCursorPosition();
+            Console.BackgroundColor = ConsoleColor.Black;
+            buffer = getBuffer(numbers[count]);
+            if(numbers[count] == 100)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+            else if(numbers[count] == 5 || numbers[count] == 15)
+                Console.BackgroundColor = ConsoleColor.Green;
+            else if (count % 2 == 0)
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("     " + numbers[count]+ buffer);
+            count ++;
+            if (count >= 20)
+                count = count-20;
+        }
+        Thread.Sleep(result);
+        start ++;
+        if (start >= 20)
+            start = start - 20;
+    }
+    int number = 0;
+    if (start + 2 >= 20)
+        number = start + 2 - 20;
+    else    
+        number = start + 2;
+    Console.BackgroundColor = ConsoleColor.Black;
+    return numbers[number];
+}
+static string getBuffer(int num)
 {
     string buffer = "";
     string phrase = num.ToString();
@@ -81,7 +135,6 @@ string getBuffer(int num)
     //Console.WriteLine ($"phrase = {phrase} phrase.Length = {phrase.Length} buffer.Length = {buffer.Length}");
     return buffer;
 }
-
 static bool drawArrow (bool tick, (int,int) position)
 {
     Console.SetCursorPosition(14,3);
